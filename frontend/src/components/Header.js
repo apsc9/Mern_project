37ca878358c7e93326d55ec765eb4c45dab6833f
aditya <mp7020216@gmail.com>
2021-07-02 14:40:18 +0530

    user register  reducer , action and screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,42 +16,41 @@ const Header = () => {
 
     return (
       <header>
-        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-            <Container>
-              <LinkContainer to='/'>
-                <Navbar.Brand>The Kroshey</Navbar.Brand>
-              </LinkContainer>
-                
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ml-auto">
-                    <LinkContainer to='/cart'>
-                      <Nav.Link>
-                        <i className='fas fa-shopping-cart'></i>Cart
-                      </Nav.Link>
-                    </LinkContainer>
+        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>ProShop</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='ml-auto'>
+                <LinkContainer to='/cart'>
+                  <Nav.Link>
+                    <i className='fas fa-shopping-cart'></i> Cart
+                  </Nav.Link>
+                </LinkContainer>
 
-                    {userInfo ? (
-                      <NavDropdown title={userInfo.name} id='username'>
-                        <LinkContainer to='/profile'>
-                          <NavDropdown.Item>Profile</NavDropdown.Item>
-                        </LinkContainer>
-                        <NavDropdown.Item onClick={logoutHandler}>
-                          Logout
-                        </NavDropdown.Item>
-                      </NavDropdown>
-                    ) : (<LinkContainer to='/login'>
-                    <Nav.Link>
-                      <i className='fas fa-user'></i>Sign In
-                    </Nav.Link>
-                  </LinkContainer>)}
-
-                  </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-    )
+  )
 }
-
 export default Header
