7b99c091f70e7f61e8b6f295ec51ab9f449912af
aditya <mp7020216@gmail.com>
2021-07-02 20:28:00 +0530

    added update user profile functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,7 @@ import {
     userLoginReducer,
     userRegisterReducer,
     userDetailsReducer,
-    //userUpdateProfileReducer,
+    userUpdateProfileReducer,
   } from './reducers/userReducers'
 
 const reducer = combineReducers({
@@ -20,7 +20,7 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    //userUpdateProfile: userUpdateProfileReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
