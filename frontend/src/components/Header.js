c51d9c46b08f7f8e5fcd092c2189d8afecf61e47
aditya <mp7020216@gmail.com>
2021-07-07 13:21:27 +0530

    added admin product list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -29,7 +29,6 @@ const Header = () => {
                     <i className='fas fa-shopping-cart'></i> Cart
                   </Nav.Link>
                 </LinkContainer>
-
                 {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
                   <LinkContainer to='/profile'>
@@ -51,10 +50,10 @@ const Header = () => {
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to='admin/productlist'>
+                  <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to='admin/orderlist'>
+                  <LinkContainer to='/admin/orderlist'>
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
