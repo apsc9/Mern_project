6e103495337023afb70708ddd026b1243bf48ac3
aditya <mp7020216@gmail.com>
2021-07-14 19:32:25 +0530

    just changed INR to rupee symbol in product component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -24,7 +24,7 @@ const Product = ({ product }) => {
                     />
                 </Card.Text>
 
-                <Card.Text as='h3'>INR{product.price}</Card.Text>
+                <Card.Text as='h3'>₹{product.price}</Card.Text>
             </Card.Body>
             
         </Card>
